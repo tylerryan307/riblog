3b89adda24c9bbee82959c5e9625d84f0a0b9e0b
Ryan Ivy <ryanivy@Ryans-MacBook-Pro.local>
2016-03-03 07:46:58 -0600

    Final working version with paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,19 +46,6 @@ class PostsController < ApplicationController
 	private
 
 	def post_params
-		params.require(:post).permit(:title, :body)
-	end
-
-	def create
-  	@user = User.create( user_params )
-	end
-
-	private
-
-	# Use strong_parameters for attribute whitelisting
-	# Be sure to update your create() and update() controller methods.
-
-	def user_params
-  	params.require(:user).permit(:avatar)
+		params.require(:post).permit(:title, :body, :image)
 	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,6 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: {minimum: 5}
 	validates :body,  presence: true
-end
-
-class User < ActiveRecord::Base
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
